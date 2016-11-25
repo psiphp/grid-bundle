7ddd222fb2283d452d6b1221f58d57d5cb437758
dantleech <dan.t.leech@gmail.com>
2016-11-25 14:13:55 +0000

    Upgrading

--- src/DependencyInjection/Compiler/CellPass.php
+++ src/DependencyInjection/Compiler/CellPass.php
@@ -7,8 +7,8 @@ class CellPass extends AbstractRegistryPass
     public function __construct()
     {
         parent::__construct(
-            'psi_grid.cell_registry',
-            'psi_grid.cell'
+            'psi_grid.column_registry',
+            'psi_grid.column'
         );
     }
 }
